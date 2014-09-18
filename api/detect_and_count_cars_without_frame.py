cf3cbaf15e5c76e16811f4db36f40c8367d499d0
John Wamburu <johnwamburu@users.noreply.github.com>
2014-09-18 16:49:06 +0300

    Update detect_and_count_cars_without_frame.py

--- api/detect_and_count_cars_without_frame.py
+++ api/detect_and_count_cars_without_frame.py
@@ -26,27 +26,3 @@ Because I doubt that my server will allow me to run a continous script forever
 With the 300 CPU-seconds I have available
 Taking a hit on performance? Maybe, but I am yet to verify
 """
-def analyze_image(_image):
-    src = os.path.join(BASE_DIR, "media/images/%s" % _image)
-    dst = os.path.join(BASE_DIR, "media/images/temp/image_00001.jpg")
-    temp_dst = os.path.join(BASE_DIR, "media/images/temp/")
-    shutil.copyfile(src, dst)
-    my_cars = 0
-    my_images = '/home/john.wamburu/api/live_traffic/media/images/temp/image_%05d.jpg'
-    cap = cv2.VideoCapture(my_images)
-    # Get my classifier ready ...
-    car_cascade = cv2.CascadeClassifier(r'/home/john.wamburu/demo/cars3.xml')
-    frames = 1
-    for f in xrange(frames):
-        ret, image = cap.read()
-        # Crop so that only the road remain, eliminate what we do not want. Get back to that.
-        #image = image[120:,:-20]
-        cars = car_cascade.detectMultiScale(image, 1.008, 5)
-        for (x,y,w,h) in cars:
-            cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)    
-        print 'Processing %d : Cars Detected : --%s--' % (f, len(cars))
-        my_cars += len(cars) # just in case we add more frames we do not lose count
-    cap.release()
-    cv2.destroyAllWindows()
-    #os.remove(dst)
-    return my_cars
